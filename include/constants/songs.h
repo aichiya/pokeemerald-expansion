fe6cf36cfd1531ceee63189886a8747e9a2a7211
aichiya <59616111+aichiya@users.noreply.github.com>
2024-08-27 06:51:52 +0700

    Setting
    Songs

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -1450,8 +1450,9 @@
 #define MUS_TH_IRO_WA_NIOEDO 1479
 #define MUS_TH_PRIMAL_SCENE_SOLO 1480
 #define MUS_TH_KIMI_TO_MATA_YUME_NO_NAKA_DE 1481
+#define MUS_TH_KIMI_TO_MATA_YUME_NO_NAKA_DE_TEST 1482
 
-#define END_MUS 					MUS_TH_KIMI_TO_MATA_YUME_NO_NAKA_DE
+#define END_MUS 					MUS_TH_KIMI_TO_MATA_YUME_NO_NAKA_DE_TEST
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
